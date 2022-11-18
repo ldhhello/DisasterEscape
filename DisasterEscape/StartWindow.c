e792e3beca2ea47468e0cacf534ef641eaad6804
Donghyun <ldhhello@naver.com>
2022-11-18 21:46:32 +0900

    코드 정리

--- DisasterEscape/StartWindow.c
+++ DisasterEscape/StartWindow.c
@@ -13,46 +13,22 @@ int current_idx = 0;
 
 void StartWindow_draw()
 {
-	//image_layer.renderAll(&image_layer);
-	//image_layer.startRender(&image_layer);
 	image_layer.imageCount = 1;
 
 	RECT rect = { SCREEN_X*16/2 - 300, SCREEN_Y * 16 / 2 + 200, SCREEN_X * 16 / 2 + 300, SCREEN_Y * 16 / 2 + 600 };
 
-	//Rectangle_(image_layer.bufferDC, rect.left, rect.top, rect.right, rect.bottom);
-
-	Image im = { "", SCREEN_X * 16 / 2, SCREEN_Y * 16 / 2 + 200, 2, 0, bitmap_button[0][0], true };
-	//Image im = { "", 10, 10, 2, 0, bitmap_button[0][0] };
+	Image im = { "", SCREEN_X * 16 / 2, SCREEN_Y * 16 / 2 + 200, 1.5, 0, bitmap_button[0][0], true };
 
 	for (int i = 0; i < menu_cnt; i++)
 	{
-		//COLORREF color = RGB(0, 0, 0);
-
-		if (current_idx == i)
-		{
-			//color = RGB(255, 0, 0);
-			im.bitmap = bitmap_button[i][1];
-
-			//Rectangle_(image_layer.bufferDC, SCREEN_X*16 / 2 - 160, SCREEN_Y*16 / 2 + 220 + 120 * i,
-				//SCREEN_X*16 / 2 + 160, SCREEN_Y*16 / 2 + 300 + 120 * i);
-
-		}
-		else
-		{
-			im.bitmap = bitmap_button[i][0];
-		}
+		im.bitmap = bitmap_button[i][current_idx == i ? 1 : 0];
 
 		image_layer.appendImage(&image_layer, im, false);
 
-		im.y += 150;
-
-		//printText(image_layer.bufferDC, SCREEN_X*16 / 2 - 200, SCREEN_Y*16 / 2 + 220 + 120*i, 
-			//SCREEN_X*16 / 2 + 200, SCREEN_Y*16 / 2 + 300 + 120*i,
-			//"¸¼Àº °íµñ", 60, color, DT_CENTER, menu_text[i]);
+		im.y += 120;
 	}
 
 	image_layer.renderAll(&image_layer);
-	//image_layer.endRender(&image_layer);
 }
 
 void StartWindow_select();
