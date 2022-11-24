18c46f4d453db82387bc2ab7c44d80691526b80f
Donghyun <ldhhello@naver.com>
2022-11-24 15:40:17 +0900

    저장 화면 만드는중...

--- DisasterEscape/resource.h
+++ DisasterEscape/resource.h
@@ -53,15 +53,16 @@
 #define IDB_BITMAP_GANGDANG_INSIDE      155
 #define IDB_BITMAP_UNDONGJANG           156
 #define IDB_BITMAP_HEALTHJANG           157
-#define IDB_BITMAP1                     158
 #define IDB_BITMAP_BIGGANGDANG          158
+#define IDB_BITMAP1                     159
+#define IDB_BITMAP_SAVE                 159
 #define I                               65535
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        159
+#define _APS_NEXT_RESOURCE_VALUE        161
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
